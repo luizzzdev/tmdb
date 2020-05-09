df4554f8e71fa77086384e4da59883692dd875fd
Luiz Fernando Rodrigues <luiiz_roodrigues@hotmail.com>
2020-05-09 19:20:09 -0300

    ajustando carousel

--- src/app/layout-components/carousel-backdrop-path/carousel-backdrop-path.component.ts
+++ src/app/layout-components/carousel-backdrop-path/carousel-backdrop-path.component.ts
@@ -1,30 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
-import {Movie} from '../../shared/Movie';
+import { Movie } from '../../shared/Movie';
 
 @Component({
   selector: 'app-carousel-backdrop-path',
   templateUrl: './carousel-backdrop-path.component.html',
-  styleUrls: ['./carousel-backdrop-path.component.sass']
+  styleUrls: ['./carousel-backdrop-path.component.sass'],
 })
 export class CarouselBackdropPathComponent implements OnInit {
+  @Input() movies: Array<Movie>;
 
   constructor(private moviesService: MoviesService) {}
 
   movie = {} as Movie;
-  movies: Movie[];
 
-  ngOnInit(): void {
-    this.getMovies();
-  }
+  ngOnInit(): void {}
 
-  getMovies() {
-    this.moviesService.getMovies().subscribe(resp => this.movies = (resp.results));
-  }
-
-  pic1='https://image.tmdb.org/t/p/original/5BwqwxMEjeFtdknRV792Svo0K1v.jpg';
-  pic2='https://image.tmdb.org/t/p/original/1R6cvRtZgsYCkh8UFuWFN33xBP4.jpg';
-  pic3='https://image.tmdb.org/t/p/original/9sXHqZTet3Zg5tgcc0hCDo8Tn35.jpg';
-  backdrop_path = 'https://image.tmdb.org/t/p/original/5BwqwxMEjeFtdknRV792Svo0K1v.jpg';
-  moviePic = 'https://image.tmdb.org/t/p/original/5BwqwxMEjeFtdknRV792Svo0K1v.jpg';
+  pic1 = 'https://image.tmdb.org/t/p/original/5BwqwxMEjeFtdknRV792Svo0K1v.jpg';
+  pic2 = 'https://image.tmdb.org/t/p/original/1R6cvRtZgsYCkh8UFuWFN33xBP4.jpg';
+  pic3 = 'https://image.tmdb.org/t/p/original/9sXHqZTet3Zg5tgcc0hCDo8Tn35.jpg';
+  backdrop_path =
+    'https://image.tmdb.org/t/p/original/5BwqwxMEjeFtdknRV792Svo0K1v.jpg';
+  moviePic =
+    'https://image.tmdb.org/t/p/original/5BwqwxMEjeFtdknRV792Svo0K1v.jpg';
 }
